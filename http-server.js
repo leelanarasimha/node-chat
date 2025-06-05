7a8b8d1efa8802bcd136d007ab7e0c466f886bee
leelawebdev <leelawebdev@gmail.com>
2025-06-05 09:10:47 +0530

    dsdsdsd

---
+++ http-server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hi there');
+});
+
+server.listen(80, '127.0.0.1', () => {
+  console.log('Server running on http://127.0.0.1:80');
+});
---
+++ simple-dns/simple-dns.js
@@ -0,0 +1,12 @@
+const dns = require('dns/promises');
+
+(async () => {
+  try {
+    const result = await dns.lookup('google.com', {
+      family: 4
+    });
+    console.log(result);
+  } catch (e) {
+    console.log('Error', e);
+  }
+})();
