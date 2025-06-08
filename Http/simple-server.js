21bc254c0bbfc76aaa15485288cc23934d4ae9ca
leelawebdev <leelawebdev@gmail.com>
2025-06-08 20:32:34 +0530

    Add simple HTTP server implementation with request logging

---
+++ Http/simple-server.js
@@ -0,0 +1,25 @@
+const http = require('node:http');
+
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+  //Method, Url, Headers, Body
+  console.log('Request Method....', request.method);
+
+  console.log('URL....', request.url);
+
+  console.log('Headers...');
+  console.log(request.headers);
+
+  request.on('data', (chunk) => {
+    console.log(chunk);
+  });
+
+  request.on('end', () => {
+    console.log('body completed');
+  });
+});
+
+server.listen(8050, () => {
+  console.log('Server is listening to http://localhost:8050');
+});
