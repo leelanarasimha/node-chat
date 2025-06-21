bf07562fefe5fa6d46b5dd10a8cda625a18a71cd
leelanarasimha <leela.narsimha@gmail.com>
2025-06-21 18:57:53 +0530

    Enhance token authentication middleware and streamline user login process

--- Http/Blogs/server.js
+++ Http/Blogs/server.js
@@ -15,21 +15,48 @@ const SESSIONS = [];
 
 const USERS = [{ id: 1, name: 'leela', username: 'leela', password: 'leela' }];
 
+//Token authentication middleware
 leelaJs.beforeEach((req, res, next) => {
-  console.log('first middleware called');
-  next();
-});
-leelaJs.beforeEach((req, res, next) => {
-  console.log('second middleware called');
-  setTimeout(() => {
+  const routesToAuthenticate = ['GET /api/user', 'PUT /api/user', 'POST /api/posts', 'DELETE /api/logout'];
+  if (routesToAuthenticate.indexOf(`${req.method} ${req.url}`) !== -1) {
+    if (req.headers.cookie) {
+      const cookies = req.headers.cookie;
+      const cookieArray = cookies.split(';');
+      let cookieData = {};
+      for (let cookie of cookieArray) {
+        const singleCookie = cookie.split('=');
+        cookieData[singleCookie[0].trim()] = singleCookie[1].trim();
+      }
+
+      const token = cookieData['token'];
+      const session = SESSIONS.find((sess) => sess.token === token);
+      if (session) {
+        req.userId = session.userId;
+        return next();
+      }
+    }
+    return res.status(401).json({ error: 'Unauthorized' });
+  } else {
     next();
-  }, 2000);
+  }
 });
+
+//Parse json body
 leelaJs.beforeEach((req, res, next) => {
-  console.log('third middleware called');
-  setTimeout(() => {
+  const contentType = req.headers['Content-Type'];
+  if (contentType === 'application/json') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString('utf8');
+    });
+    req.on('end', () => {
+      body = JSON.parse(body);
+      req.body = body;
+      return next();
+    });
+  } else {
     next();
-  }, 2000);
+  }
 });
 
 leelaJs.route('get', '/api/posts', (req, res) => {
@@ -44,55 +71,37 @@ leelaJs.route('get', '/api/posts', (req, res) => {
 });
 
 leelaJs.route('post', '/api/login', (req, res) => {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk.toString('utf8');
-  });
-  req.on('end', () => {
-    const reqBody = JSON.parse(body);
-    const username = reqBody.username;
-    const password = reqBody.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const userDetails = USERS.find((user) => user.username === username);
+  const userDetails = USERS.find((user) => user.username === username);
 
-    if (userDetails && userDetails.password === password) {
-      const token = Math.floor(Math.random() * 10000000000).toString();
+  if (userDetails && userDetails.password === password) {
+    const token = Math.floor(Math.random() * 10000000000).toString();
 
-      SESSIONS.push({
-        userId: userDetails.id,
-        token
-      });
-      res.setHeader('Set-Cookie', [`token=${token}; Path=/`, `user=Leelawebdev; Path=/`]);
-      //res.setHeader('Set-Cookie', `user=Leelawebdev; Path=/`);
-      res.json({ message: 'Successfully Logged in' });
-    } else {
-      res.status(401).json({ error: 'Invalid username or password' });
-    }
-  });
+    SESSIONS.push({
+      userId: userDetails.id,
+      token
+    });
+    res.setHeader('Set-Cookie', [`token=${token}; Path=/`, `user=Leelawebdev; Path=/`]);
+    //res.setHeader('Set-Cookie', `user=Leelawebdev; Path=/`);
+    res.json({ message: 'Successfully Logged in' });
+  } else {
+    res.status(401).json({ error: 'Invalid username or password' });
+  }
 });
 
 leelaJs.route('get', '/api/user', (req, res) => {
-  const cookies = req.headers.cookie;
-  const cookieArray = cookies.split(';');
-  let cookieData = {};
-  for (let cookie of cookieArray) {
-    const singleCookie = cookie.split('=');
-    cookieData[singleCookie[0].trim()] = singleCookie[1].trim();
-  }
+  const user = USERS.find((singleUser) => singleUser.id === req.userId);
+  res.json(user);
 
-  const token = cookieData['token'];
-
-  const session = SESSIONS.find((sess) => sess.token === token);
-  console.log(session);
-  console.log(SESSIONS);
-  if (session) {
-    const user = USERS.find((singleUser) => singleUser.id === session.userId);
-    res.json(user);
-  } else {
-    res.status(401).json({ error: 'Invalid user data' });
-  }
   //send the current logged in user details
 });
+
+leelaJs.route('put', '/api/user', (req, res) => {});
+
+leelaJs.route('post', '/api/posts', (req, res) => {});
+leelaJs.route('delete', '/api/logout', (req, res) => {});
 
 leelaJs.listen(8000, () => {
   console.log(`Server is listening at port 8000`);
