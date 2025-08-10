b9d8bad31625ac60d2aa415fd5ed6cf07c57cc0e
leelanarasimha <leela.narsimha@gmail.com>
2025-08-10 13:29:24 +0530

    Implement worker threads in app.js and add calc.js for computation

--- threads/app.js
+++ threads/app.js
@@ -1,3 +1,8 @@
-setInterval(() => {
-  console.log('Hey');
-}, 5000);
+const { Worker } = require('node:worker_threads');
+
+const a = 400;
+console.log(a);
+
+for (let i = 0; i < 10; i++) {
+  new Worker('./calc.js');
+}
---
+++ threads/calc.js
@@ -0,0 +1,4 @@
+const a = 1000;
+console.log(a);
+
+while (true) {}
