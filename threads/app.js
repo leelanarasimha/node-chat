1fcb0d2c8d6cdaa5e8ad679ddd13e3488d72a67d
leelanarasimha <leela.narsimha@gmail.com>
2025-09-16 09:05:35 +0530

    Refactor example code in app.js to demonstrate MessageChannel usage

--- threads/app.js
+++ threads/app.js
@@ -1,7 +1,25 @@
 const { Worker } = require('node:worker_threads');
 
-const obj = { name: 'john' };
+// Example1: Sending Data with WorkerData
+// const obj = { name: 'john' };
 
-const worker = new Worker('./calc.js', { workerData: obj });
+// const worker = new Worker('./calc.js', { workerData: obj });
 
-console.log('main thread: ', obj.name);
+// console.log('main thread: ', obj.name);
+
+//Example2: Sending data using MessageChannel
+const messageChannel = new MessageChannel();
+
+const port1 = messageChannel.port1;
+const port2 = messageChannel.port2;
+
+port1.postMessage('Port2 John');
+port2.postMessage('port1 joe');
+
+port2.on('message', (msg) => {
+  console.log('port2 received the message', msg);
+});
+
+port1.on('message', (msg) => {
+  console.log('port1 received the message', msg);
+});
