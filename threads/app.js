7ff477103b4d637f3c11941a47f0c283bda87829
leelanarasimha <leela.narsimha@gmail.com>
2025-09-16 23:07:24 +0530

    Refactor MessageChannel implementation in app.js and calc.js for improved worker communication

--- threads/app.js
+++ threads/app.js
@@ -8,18 +8,24 @@ const { Worker } = require('node:worker_threads');
 // console.log('main thread: ', obj.name);
 
 //Example2: Sending data using MessageChannel
-const messageChannel = new MessageChannel();
+// const messageChannel = new MessageChannel();
 
-const port1 = messageChannel.port1;
-const port2 = messageChannel.port2;
+// const port1 = messageChannel.port1;
+// const port2 = messageChannel.port2;
 
-port1.postMessage('Port2 John');
-port2.postMessage('port1 joe');
+// port1.postMessage('Port2 John');
+// port2.postMessage('port1 joe');
 
-port2.on('message', (msg) => {
-  console.log('port2 received the message', msg);
-});
+// port2.on('message', (msg) => {
+//   console.log('port2 received the message', msg);
+// });
 
-port1.on('message', (msg) => {
-  console.log('port1 received the message', msg);
-});
+// port1.on('message', (msg) => {
+//   console.log('port1 received the message', msg);
+// });
+
+//Example3: Communication between two worker threads
+const { port1, port2 } = new MessageChannel();
+
+const thread1 = new Worker('./calc.js', { workerData: { port: port1 }, transferList: [port1] });
+const thread2 = new Worker('./calc.js', { workerData: { port: port2 }, transferList: [port2] });
--- threads/calc.js
+++ threads/calc.js
@@ -1,5 +1,9 @@
 const { workerData } = require('node:worker_threads');
-console.log('This is the worker thread');
-workerData.name = 'Leela';
 
-console.log('WOrker Data: ', workerData.name);
+const port = workerData.port;
+
+port.on('message', (msg) => {
+  console.log('message received', msg);
+});
+
+port.postMessage('sending text');
